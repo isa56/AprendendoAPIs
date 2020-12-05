5080367f7c05bab9302fb2011c8a38a2865f30bc
Isadora Ferreira <isadoraadriana@gmail.com>
2020-12-05 00:15:18 -0300

    initial commit

---
+++ app/src/main/java/com/isa56/aprendendoapis/Cat.kt
@@ -0,0 +1,9 @@
+package com.isa56.aprendendoapis
+
+class Cat (
+    var id: String,
+    var url: String,
+    var breeds: List<Any>,
+    var width: Int,
+    var height: Int
+)
---
+++ app/src/main/java/com/isa56/aprendendoapis/Dog.kt
@@ -0,0 +1,6 @@
+package com.isa56.aprendendoapis
+
+class Dog (
+    var message: String,
+    var status: String
+)
