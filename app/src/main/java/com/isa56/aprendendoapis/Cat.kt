f0ade818d442913438e53bf6b303e5912a810935
Isadora Ferreira <isadoraadriana@gmail.com>
2020-12-06 13:13:19 -0300

    Agora ele funciona :D

--- app/src/main/java/com/isa56/aprendendoapis/Cat.kt
+++
@@ -1,9 +0,0 @@
-package com.isa56.aprendendoapis
-
-class Cat (
-    var id: String,
-    var url: String,
-    var breeds: List<Any>,
-    var width: Int,
-    var height: Int
-)
--- app/src/main/java/com/isa56/aprendendoapis/activity/MainActivity.kt
+++ app/src/main/java/com/isa56/aprendendoapis/activity/MainActivity.kt
@@ -1,16 +1,24 @@
-package com.isa56.aprendendoapis
+package com.isa56.aprendendoapis.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.isa56.aprendendoapis.R
+import com.isa56.aprendendoapis.classes.Cat
+import com.isa56.aprendendoapis.classes.Dog
+import com.isa56.aprendendoapis.services.CatService
+import com.isa56.aprendendoapis.services.DogService
 import com.squareup.picasso.Picasso
+import org.json.JSONArray
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -57,7 +65,8 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFailure(call: Call<Dog>, t: Throwable) {
                 Log.i("dogError", t.toString())
-            }})
+            }
+        })
     }
 
     private fun catFunction(){
@@ -67,20 +76,21 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         val catService: CatService = retrofitCat.create(CatService::class.java)
-        val callCat = catService.randomCat().enqueue(object:Callback<Cat>{
-            override fun onResponse(call: Call<Cat>, response: Response<Cat>) {
+        val callCat = catService.randomCat().enqueue(object : Callback<List<Cat>> {
+            override fun onResponse(call: Call<List<Cat>>, response: Response<List<Cat>>) {
                 val randomCat = response.body()!!
-
-                Log.i("catMessage", randomCat.url)
+                
+                Log.i("catMessage", randomCat[0].url)
 
                 Picasso.get()
-                    .load(randomCat.url)
+                    .load(randomCat[0].url)
                     .into(imgView)
             }
 
-            override fun onFailure(call: Call<Cat>, t: Throwable) {
+            override fun onFailure(call: Call<List<Cat>>, t: Throwable) {
                 Log.i("catError", t.toString())
-            }})
+            }
+        })
     }
 
 }
